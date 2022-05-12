15d57fe45792e0c9aad0c0c90d7dff43383c16b1
Badr Choubai <badrchoubai@Badrs-MacBook-Pro.local>
2022-05-12 14:58:54 -0600

    Display settings moved to Parent

--- components/Navigation/AppHeader.vue
+++ components/Navigation/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
-<header class="app-header" role="banner">
-    <a href="#main" class="skip-to-main sr-only">Skip to main content</a>
-    <div class="container mx-auto p-4">
-        <h1 class="font-display font-black dark:text-white text-3xl">HACKMSUD</h1>
-        <navigation-app-menu />
-    </div>
-</header>
+    <header>
+        <a href="#main" aria-label="skip to main content" class="skip-to-main sr-only">Skip to main content</a>
+        <div class="container mx-auto pt-4 px-4">
+            <h1 class="font-display font-black dark:text-white text-3xl">HACKMSUD</h1>
+            <navigation-app-menu />
+        </div>
+    </header>
 </template>

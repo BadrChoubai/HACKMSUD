7299e81e1cfd2c15af3411c6e879cc1e69a3de3c
Badr Choubai <badrchoubai@Badrs-MBP.hsd1.co.comcast.net>
2022-05-19 12:02:15 -0600

    Fix button container styles on index page

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,11 @@
             <p class="mt-3 mb-3">
                 We are going to be planning MSU Denver's very first Hackathon event and will need interested students to join us in doing so, information will be posted somewhere at some time. Stay up to date on the CS Student Discord and through your student emails!
             </p>
-            <div id="BtnContainer" class="mt-3 flex flex-row justify-start flex-wrap gap-4">
-                <button class="bg-blue-600/80 dark:bg-red-500/80 p-4 rounded">
+            <div id="BtnContainer" class="flex flex-row flex-wrap gap-4 mt-3 mb-3">
+                <button class="bg-blue-600/80 dark:bg-red-500/80 p-4 rounded md:w-1/3 w-full">
                     <nuxt-link class="text-white tracking-widest whitespace-nowrap font-display font-black drop-shadow-md" to="/hackathon" aria-label="Learn More">LEARN MORE</nuxt-link>
                 </button>
-                <button class="bg-blue-600/80 dark:bg-red-500/80 p-4 rounded">
+                <button class="bg-blue-600/80 dark:bg-red-500/80 p-4 rounded md:w-1/3 w-full">
                     <a class="text-white tracking-widest whitespace-nowrap font-display text-clip font-black drop-shadow-md" href="https://discord.gg/PpGv3naY" aria-label="Join us on Discord">JOIN US ON DISCORD</a>
                 </button>
             </div>

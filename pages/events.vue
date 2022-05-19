c3ba81432b4cd01ff38351aeedc01ca09e4124c6
Badr Choubai <badrchoubai@Badrs-MBP.hsd1.co.comcast.net>
2022-05-19 13:59:26 -0600

    Hydrate Events Page

--- pages/events.vue
+++ pages/events.vue
@@ -1,7 +1,64 @@
+<script setup lang="ts">
+interface Event {
+    title: string;
+    description: string;
+    date?: Date ;
+    url: string;
+    tags: string[];
+}
+
+const events: Event[] = [
+    {
+        title: "Das42 Presentation",
+        description: "Das42 is a startup company in Denver",
+        date: new Date("2022-04-28"),
+        url: "https://www.msudenver.edu/career-link",
+        tags: ["on-campus", "ACM"]
+    },
+        {
+        title: "HACKMSUD",
+        description: "Planning is currently in progress for MSU Denver's first ever Hackathon.",
+        url: "https://discord.gg/PpGv3naY",
+        tags: ["on-campus", "ACM"]
+    },
+]
+</script>
+
 <template>
-    <section>
-        <div class="container mx-auto">
-            <h1>Events</h1>
-        </div>
-    </section>
+    <div class="flex flex-col gap-8 lg:w-5/6">
+        <section>
+            <article>
+                <h1 class="mb-2 border-b-4 border-b-blue-500 dark:border-b-red-500">Events</h1>
+                <p class="text-2xl">
+                    The ACM Student Association plans to hold events for the Fall Semester, and also wants to start
+                    attending tech events in the broader Denver Metro Tech community.
+                    Below are all the events that we plan on hosting ourselves or attending as a group.
+                </p>
+            </article>
+        </section>
+        <section>
+            <header>
+                <h2 class="font-bold">Events Calendar</h2>
+            </header>
+            <div class="md:mt-4 flex flex-row flex-wrap items-stretch gap-4">
+                <article class="flex flex-col justify-around items-stretch p-2 shadow-sm md:w-5/12 w-full rounded-md dark:bg-slate-700 bg-white/50" v-for="event in events">
+                    <header class="block">
+                        <nuxt-link :to="event.url" external :aria-label="`View event website for ${event.title}`">
+                            <h3 class="text-xl font-semibold underline underline-offset-4">{{ event.title }}</h3>
+                        </nuxt-link>
+                        <span v-if="event.date">{{ event.date.toDateString() }}</span>
+                        <span v-else>TBA</span>
+                    </header>
+                    <p class="text-lg mt-2 mb-2">
+                        {{ event.description }}
+                    </p>
+                    <div class="flex items-center flex-row gap-2 mt-2 mb-2">
+                        <span class="bg-blue-400/20 dark:bg-red-400/20 px-2 whitespace-nowrap" v-for="tag in event.tags" :key="tag">
+                            {{ tag.toUpperCase() }}
+                        </span>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
             <h1 class="font-display font-black lg:text-8xl border-b-blue-600 dark:border-b-red-500 border-b-4 mb-3">
                 MSU Denver's First Hackathon
             </h1>
-            <p class="mt-3 mb-3">
+            <p class="mt-3 mb-3 text-2xl">
                 This is the homepage for MSU Denver's official Hackathon, led by students for students. It's open-source and wholley meant to help students learn about and stay up to date on all things pertaining to MSU Denver, campus- and city-wide tech events.
             </p>
-            <p class="mt-3 mb-3">
+            <p class="mt-3 mb-3 text-2xl">
                 We are going to be planning MSU Denver's very first Hackathon event and will need interested students to join us in doing so, information will be posted somewhere at some time. Stay up to date on the CS Student Discord and through your student emails!
             </p>
             <div id="BtnContainer" class="flex flex-row flex-wrap gap-4 mt-3 mb-3">
